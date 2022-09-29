2b188142a30c75b70b434de08c894af3a8666101
luisgcl <37517860+luisgcl@users.noreply.github.com>
2022-09-29 18:21:49 -0500

    Proyecto Criptomonedas terminado
    Finalizando los componentes del proyecto de Criptomonedas

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Resultados = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Resultado = ({ resultado }) => {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+  return (
+    <Resultados>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio mas alto del dia: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio mas bajo del dia: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Ultima actualizacion: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Resultados>
+  );
+};
+
+export default Resultado;
